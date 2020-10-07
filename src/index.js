c23fdd30dbe20073e671d31fbde2905b1ee39886
Tim Sorokin <timsorokin352@gmail.com>
2020-10-07 13:05:36 +0300

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let result = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+    let str = expr.substr(i, 10);
+    result.push(str);
+  }
+
+  return result
+    .map((elem) => {
+      let temp = "";
+      for (let i = 0; i < elem.length; i += 2) {
+        let str = elem.substr(i, 2);
+        if (str === "10") {
+          temp += ".";
+        } else if (str === "11") {
+          temp += "-";
+        }
+      }
+      return MORSE_TABLE[temp] ? MORSE_TABLE[temp] : " ";
+    })
+    .join("");
 }
 
 module.exports = {
